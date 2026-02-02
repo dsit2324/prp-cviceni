bc7b7a248c908495d4fce5223ce4e50d1a448233
ds <148313992+dsit2324@users.noreply.github.com>
2026-02-02 21:55:33 +0100

    Update Datum.cpp

--- Datum.cpp
+++ Datum.cpp
@@ -1,32 +1,116 @@
-#include <iostream>
-#include <limits>
-#include "Datum.h"
+//
+// Created by dsofc on 01.02.2026.
+//
 
-int main() {
-    Datum d1;
-    Datum d2(1, 1, 2000);
-    Datum d3(d2);
-    std::cout << "Objekt vytvoreny vychozim konstruktorem d1: " << d1 << std::endl;
-    std::cout << "Objekt vytvoreny vychozim konstruktorem d2: " << d2 << std::endl;
-    std::cout << "Objekt vytvoreny vychozim konstruktorem d3: " << d3 << std::endl;
-    std::cout << "Zadani noveho data" << std::endl;
-    int den;
-    int mesic;
-    int rok;
-    do {
-        std::cout << "Zadej den:" << std::endl;
-        std::cin >> den;
-        std::cout << "Zadej mesic:" << std::endl;
-        std::cin >> mesic;
-        std::cout << "Zadej rok:" << std::endl;
-        std::cin >> rok;
-        Datum d1(den, mesic, rok);
-        if (!d1.setDatum(den, mesic, rok) || std::cin.fail() || std::cin.peek() != '\n') {
-            std::cout << "Nebylo zadano platne datum, zadej znovu:" << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-    } while (!d1.setDatum(den, mesic, rok));
-    std::cout << "Objekt d1 po zmene: " << d1 << std::endl;
-    return 0;
+#include "Datum.h"
+#include <ctime>
+
+Datum::Datum() {
+    systemoveDatum();
+}
+
+Datum::Datum(int den, int mesic, int rok) {
+    if (!setDatum(den, mesic, rok)) {
+        systemoveDatum();
+    }
+}
+
+Datum::Datum(const Datum &kopirka) : den(kopirka.getDen()), mesic(kopirka.getMesic()), rok(kopirka.getRok()) {
+}
+
+Datum::~Datum() {
+}
+
+int Datum::getDen() const {
+    return this->den;
+}
+
+int Datum::getMesic() const {
+    return this->mesic;
+}
+
+int Datum::getRok() const {
+    return this->rok;
+}
+
+bool Datum::setDen(int den) {
+    if (den < 1 || den > pocetDniVmesic(mesic, rok)) {
+        return false;
+    } else {
+        this->den = den;
+        return true;
+    }
+}
+
+bool Datum::setMesic(int mesic) {
+    if (mesic >= 1 && mesic <= 12) {
+        this->mesic = mesic;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Datum::setRok(int rok) {
+    if (rok >= MINROK && rok <= MAXROK) {
+        this->rok = rok;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Datum::setDatum(int den, int mesic, int rok) {
+    if (platnostData(den, mesic, rok)) {
+        this->den = den;
+        this->mesic = mesic;
+        this->rok = rok;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Datum::systemoveDatum() {
+    time_t t = time(nullptr);
+    tm *now = localtime(&t);
+
+    den = now->tm_mday;
+    mesic = now->tm_mon + 1;
+    rok = now->tm_year + 1900;
+}
+
+bool Datum::platnostData(int den, int mesic, int rok) const {
+    if ((rok >= MINROK && rok <= MAXROK) && (mesic >= 1 && mesic <= 12) && (
+            den >= 1 && den <= pocetDniVmesic(mesic, rok))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Datum::prestupnyRok(int rok) const {
+    if ((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0)) {
+        return true;
+    }
+    return false;
+}
+
+int Datum::pocetDniVmesic(int mesic, int rok) const {
+    if (mesic == 2 && prestupnyRok(rok)) {
+        return 29;
+    } else if (mesic == 2 && !prestupnyRok(rok)) {
+        return 28;
+    } else if (mesic == 4 || mesic == 6 || mesic == 9 || mesic == 11) {
+        return 30;
+    } else {
+        return 31;
+    }
+}
+
+std::ostream & operator<<(std::ostream &os, const Datum &d) {
+    os << d.getDen() << ". ";
+    os << d.getMesic() << ". ";
+    os << d.getRok();
+    return os;
 }
